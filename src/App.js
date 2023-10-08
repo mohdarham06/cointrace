36ad5d4670dc7207d05cac981e4e874612891d59
Arham <mohdarham0634@gmail.com>
2023-10-09 01:24:06 +0530

    pagination added

--- src/App.js
+++ src/App.js
@@ -6,46 +6,13 @@ import Market from './components/sections/Market';
 import Footer from './components/sections/Footer';
 
 
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
 
 
 function App() {
 
-    const [cryptoData, setCryptoData] = useState([]);
-
-
-    // CoinGecko Api
-    const options = {
-        method: 'GET',
-        url: 'https://coingecko.p.rapidapi.com/coins/markets',
-        params: {
-            vs_currency: 'usd',
-            page: '1',
-            per_page: '10'
-        },
-        headers: {
-            'X-RapidAPI-Key': 'd9b652efb4msh6e4040368154c25p1c68bejsn16303a787b72',
-            'X-RapidAPI-Host': 'coingecko.p.rapidapi.com'
-        }
-    }
-
-    useEffect(() => {
-        async function getMarketData() {
-            try {
-                const response = await axios.request(options);
-                setCryptoData(response.data)
-                console.log(response.data);
-
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        getMarketData()
-        // [] at load time
-    }, [])
-
-
+    
 
 
 
@@ -55,9 +22,9 @@ function App() {
             <Header />
 
             <main>
-                <Hero cryptoData={cryptoData} />
+                <Hero />
 
-                <Market cryptoData={cryptoData} />
+                <Market />
             </main>
 
 
--- src/components/sections/Market.jsx
+++ src/components/sections/Market.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import MarketUpdate from '../MarketUpdate'
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 // cryptoData from App
-const Market = ({ cryptoData }) => {
+const Market = () => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const [cryptoData, setCryptoData] = useState([]);
+
+
+    // CoinGecko Api
+    const options = {
+        method: 'GET',
+        url: 'https://coingecko.p.rapidapi.com/coins/markets',
+        params: {
+            vs_currency: 'usd',
+            page: currentPage,
+            per_page: '10'
+        },
+        headers: {
+            'X-RapidAPI-Key': 'd9b652efb4msh6e4040368154c25p1c68bejsn16303a787b72',
+            'X-RapidAPI-Host': 'coingecko.p.rapidapi.com'
+        }
+    }
+
+    useEffect(() => {
+        async function getMarketData() {
+            try {
+                const response = await axios.request(options);
+                setCryptoData(response.data)
+                console.log(response.data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getMarketData()
+        // [] at load time
+    }, [currentPage])
+
+
+    const paginationButtons = [];
+    for (let i = 1; i <= 5; i++) {
+        paginationButtons.push(
+            <button
+                key={i}
+                onClick={() => setCurrentPage(i)}
+                className={'number-pagination ' + (i === currentPage ? 'activePagi' : '')}
+            >
+                {i}
+            </button>
+        )
+    }
+
 
 
     return (
@@ -12,8 +63,26 @@ const Market = ({ cryptoData }) => {
             <h2 className='section-headings'>Market Update</h2>
             <MarketUpdate cryptoData={cryptoData} />
 
-            <div className="sysstum"></div>
-            
+            <div className="pagination-container">
+
+                {/* <button className='previous-pagination'
+                    onClick={() => {
+                        if (currentPage <= 1) return;
+                        setCurrentPage(currentPage - 1)
+                    }}>
+                    Previous {currentPage}
+                </button> */}
+
+                {paginationButtons}
+
+                {/* <button className='next-pagination'
+                    onClick={() => {
+                        setCurrentPage(currentPage + 1)
+                    }}>
+                    Next {currentPage}
+                </button> */}
+            </div>
+
         </section>
     )
 }
