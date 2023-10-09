baac5ef7e9f091796a13dd4b1aa7e87e59855fd1
Arham <mohdarham0634@gmail.com>
2023-10-09 21:56:26 +0530

    footer added

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 
+import { FiInstagram } from "react-icons/fi";
+import { FiTwitter } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
+import { FiYoutube } from "react-icons/fi";
+
+// import { RiInstagramLine  } from "react-icons/ri";
+// import { RiTwitterLine } from "react-icons/ri";
+// import { RiFacebookFill } from "react-icons/ri";
+// import { RiDiscordLine } from "react-icons/ri";
+
 
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className='footer-section'>
+
+            <div className="footer-content">
+                <div className="social-icons">
+                    <FiInstagram />
+                    <FiFacebook />
+                    <FiTwitter />
+                    <FiYoutube />
+
+                    {/* <RiInstagramLine />
+                    <RiFacebookFill />
+                    <RiTwitterLine />
+                    <RiDiscordLine /> */}
 
 
+                    
+                </div>
+
+                <div className="footer-text">
+                    <p>Privacy</p>
+                    <p>Terms of Use</p>
+                </div>
+            </div>
 
 
         </footer>
