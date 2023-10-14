771319f96b7f9c536c7dc98a575d7afe60c1f727
Arham <mohdarham0634@gmail.com>
2023-10-14 23:42:00 +0530

    features section started

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
 const Features = () => {
-  return (
-    <section className='features-section'>
-        <div className="">
-            <h2 className="section-heading"></h2>
+    return (
+        <section className='features-section'>
+            <div className="">
+                <h2 className="section-heading">
+                    Why
+                    <span>We are the Best</span>
+                </h2>
 
-            <div className=""></div>
-        </div>
-    </section>
-  )
+
+                <div className=""></div>
+            </div>
+        </section>
+    )
 }
 
 export default Features
